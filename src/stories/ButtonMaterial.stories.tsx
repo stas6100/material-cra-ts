e13ad104a5891e3b3265f78fd8b4d922a05cf6ea
Stas Marchenko <stas6100@gmail.com>
2022-08-31 20:58:07 +0200

    new design

--- src/stories/ButtonMaterial.stories.tsx
+++ src/stories/ButtonMaterial.stories.tsx
@@ -20,7 +20,7 @@ export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
   variant: 'contained',
-  children: 'test',
+  children: 'test123',
 };
 
 Primary.story = {
